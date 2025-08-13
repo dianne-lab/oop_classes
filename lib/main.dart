7fd8a809d3de4103731e93ca03e5948daeaf9444
jelaxz <jelainesumayan@gmail.com>
2025-08-13 19:11:58 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'car.dart';
+import 'fruit.dart';
+void main() {
+  // Create an object
+  Fruit mango = Fruit("Mango", "Yellow");
+
+  // Call the method
+  mango.describe();
+
 void main() {
   // Create an object
   Car myCar = Car("Toyota", 850000);
 
   // Call the method
   myCar.displayInfo();
+}
 }
