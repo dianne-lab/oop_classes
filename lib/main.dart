64e193ca1a87980c4a74b1570b6236e6745790ff
dianne-lab <diannemarieibajo1@gmail.com>
2025-08-15 01:47:06 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,22 +4,15 @@ import 'animal.dart';
 void main() {
   // Create an object
   Fruit mango = Fruit("Mango", "Yellow");
-
   // Call the method
   mango.describe();
 
-void main() {
-  // Create an object
   Car myCar = Car("Toyota", 850000);
-
   // Call the method
   myCar.displayInfo();
-  void main() {
-  // Create an object
-  Animal dog = Animal("Dog", 3);
 
+  Animal dog = Animal("Dog", 3);
   // Call the method
   dog.makeSound();
-  }
 }
-}
+
