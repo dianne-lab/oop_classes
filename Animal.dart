61d2889f55eb78aeac978273351a75f60849f471
xanih <xandralindoller2005@gmail.com>
2025-08-15 15:41:24 +0800

    Create Animal.dart

---
+++ Animal.dart
@@ -0,0 +1,13 @@
+class Animal {
+  // Properties
+  String species;
+  int age;
+
+  // Constructor
+  Animal(this.species, this.age);
+
+  // Method
+  void makeSound() {
+    print("The $species makes a sound.");
+  }
+}
